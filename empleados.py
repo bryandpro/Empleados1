01617bbeab2681f5e8cece0878f2022514f44fe4
unknown <bryandpro>
2023-02-18 12:03:16 -0600

    los empleados

---
+++ empleados.py
@@ -0,0 +1,9 @@
+DPI=input("Ingrese el No. DPI ")
+Nombrecompleto=input("Ingrese el Nombre de la persona ")
+Edad=int(input("Ingrese la Edad "))
+telefono=input("Ingrese Nùmero telèfono ")
+Direccion=input("Ingrese Direcciòn ")
+Puesto=input("Ingrese Puesto") 
+
+print ("El postulante a " + Puesto + " con nombre " + Nombrecompleto + " su edad es de " + str (Edad ) + " su telefono es " + telefono + " su Direcciòn es " + Direccion)
+
